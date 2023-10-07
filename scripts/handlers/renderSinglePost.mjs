f9b2e634568aa6a6df64f3ab0c7d99497835dadf
Karen Jo <karenfiksdahl@gmail.com>
2023-10-07 13:39:33 +0200

    starting all over with fetching posts

--- scripts/handlers/renderSinglePost.mjs
+++ scripts/handlers/renderSinglePost.mjs
@@ -1,42 +1,42 @@
 
-function renderSinglePost(post) {
-    console.log(post);
-    const postTitle = document.getElementById("card-title");
-    const postBody = document.getElementById("card-body");
-    const postTags = document.getElementById("card-tags");
-    const postMedia = document.getElementById("card-media");
-    const postCreated = document.getElementById("card-created");
-    const postUpdated = document.getElementById("card-updated");
+// function renderSinglePost(post) {
+//     console.log(post);
+//     const postTitle = document.getElementById("card-title");
+//     const postBody = document.getElementById("card-body");
+//     const postTags = document.getElementById("card-tags");
+//     const postMedia = document.getElementById("card-media");
+//     const postCreated = document.getElementById("card-created");
+//     const postUpdated = document.getElementById("card-updated");
 
 
-    postTitle.textContent = post.title;
-    postBody.textContent = post.body;
+//     postTitle.textContent = post.title;
+//     postBody.textContent = post.body;
 
-    // Display tags
-    if (post.tags && post.tags.length > 0) {
-        postTags.textContent = "Tags: " + post.tags.join(", ");
-    } else {
-        postTags.textContent = "";
-    }
+//     // Display tags
+//     if (post.tags && post.tags.length > 0) {
+//         postTags.textContent = "Tags: " + post.tags.join(", ");
+//     } else {
+//         postTags.textContent = "";
+//     }
 
-// Display media/image
-if (post.media) {
-    postMedia.src = post.media;
-    postMedia.alt = "Post Media";
-    postMedia.classList.add("img-thumbnail");
-} else {
-    // clear the media if no media element
-    postMedia.src = "";
-    postMedia.alt = "";
-    postMedia.classList.remove("img-thumbnail");
-}
-
-
-// timestamps (chatGPT)
-postCreated.textContent = "Created: " + new Date(post.created).toLocaleString();
-postUpdated.textContent = "Updated: " + new Date(post.updated).toLocaleString();
-}
+// // Display media/image
+// if (post.media) {
+//     postMedia.src = post.media;
+//     postMedia.alt = "Post Media";
+//     postMedia.classList.add("img-thumbnail");
+// } else {
+//     // clear the media if no media element
+//     postMedia.src = "";
+//     postMedia.alt = "";
+//     postMedia.classList.remove("img-thumbnail");
+// }
 
 
+// // timestamps (chatGPT)
+// postCreated.textContent = "Created: " + new Date(post.created).toLocaleString();
+// postUpdated.textContent = "Updated: " + new Date(post.updated).toLocaleString();
+// }
 
-export {renderSinglePost}; 
+
+
+// export {renderSinglePost}; 
--- scripts/posts/allPosts.mjs
+++ scripts/posts/allPosts.mjs
@@ -2,22 +2,20 @@
 import { getWithToken } from "../auth/token.mjs";
 import { renderPosts } from "../handlers/renderPosts.mjs";
 import { getAllPosts } from "../helpers/API.mjs";
-import { getLatestPosts } from "../handlers/getLatestPosts.mjs";
 
-(async () => {
-    try {
-        const token = localStorage.getItem("accessToken");
+// (async () => {
+//     try {
+//         const token = localStorage.getItem("accessToken");
 
-        if (!token) {
-            throw new Error("Can't access token");
-        }
+//         if (!token) {
+//             throw new Error("Can't access token");
+//         }
 
-        const latestPosts = await getLatestPosts(20); // Change 20 to the desired post count
-        console.log("latest posts:", latestPosts);
+//         const response = await getWithToken(getAllPosts);
+//         if (!response.ok) {
+//             throw new Error(`Error: ${response.status}`);
+//         }
 
-        // Call the renderPosts function with the latest posts
-        renderPosts(latestPosts);
-    } catch (error) {
-        console.error("Error fetching and rendering latest posts:", error.message);
-    }
-})();
+//       }
+//     })
+      
